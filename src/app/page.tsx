af8d6440422551b4188ea716e8cf4446572699f9
Jonathon <jonathon@pixel8.io>
2023-07-02 14:56:50 -0400

    playing around and working on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,124 @@
 import Image from "next/image";
+import Link from "next/link";
+
+import Logo from "../assets/images/logo.svg";
+import Glows from "../assets/images/glows.svg";
+import Arrows from "../assets/images/arrows.svg";
+import Rocket from "../assets/images/rocket.svg";
+import Steps from "../assets/images/steps.svg";
+import Resources from "../assets/images/resources.svg";
+import HomeCode from "../assets/images/homecode.png";
+
+import "../assets/css/animations.scss";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx test</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div id="top">
+      <header className="py-[35px]">
+        <div className="container mx-auto flex justify-between">
+          <Image src={Logo} className="SorobanLearn" alt="logo" />
+          <div className="flex gap-[25px]">
+            <Link
+              href="https://discord.gg/xYdDBnRVK5"
+              target="_blank"
+              className="w-12 h-12 bg-white border-2 border-white hover:border-indigo-700 text-black flex justify-center items-center hover:bg-indigo-700 hover:text-white transition-all duration-500"
+              rel="noreferrer"
+            >
+              <i className="fa-brands fa-discord" />
+            </Link>
+            <Link
+              href="/exercise/hello-world"
+              className="border-2 border-white sm:flex justify-center items-center px-6 hover:bg-indigo-700 hover:text-white hover:border-indigo-700 transition-all duration-500 hidden"
+            >
+              Try Now
+            </Link>
+          </div>
         </div>
-      </div>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="py-10 sm:py-20 overflow-hidden relative">
+        <div className="container mx-auto">
+          <h1 className="text-7xl sm:text-9xl text-white font-extralight mb-16 mt-16">
+            New to <span className="font-medium">Soroban</span>?
+          </h1>
+          <div className="flex flex-col-reverse lg:flex-row justify-between">
+            <div className="w-fill lg:w-[475px] relative">
+              <Image
+                src={Arrows}
+                alt="Arrows"
+                className="absolute -z-10 -top-10 -left-14 moving-image"
+              />
+              <p className="text-xl leading-7 text-white mb-6 mt-6">
+                Soroban Learn is a tool that makes it easy for new developers to
+                get their feet wet into learning the basics of creating smart
+                contracts in Soroban.
+              </p>
+              <p className="text-lg leading-relaxed text-gray-400 mb-12">
+                It will walk you through creating your first “Hello World”
+                contract and finish up with a token swap contract.
+              </p>
+              <Link
+                href="/exercise/hello-world"
+                className="bg-indigo-700 px-10 py-3 inline-block"
+              >
+                Get Started
+              </Link>
+            </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <div>
+              <Image src={HomeCode} alt="Home Code" className="ml-auto" />
+              <Image
+                src={Glows}
+                alt="glows"
+                className="absolute top-0 right-0 opacity-60 -z-10 moving-image"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section className="py-20">
+        <div className="container mx-auto">
+          <div className="flex justify-between gap-[130px] flex-col lg:flex-row">
+            <div className="flex flex-col justify-center items-center text-center gap-5">
+              <Image src={Rocket} alt="Rocket" className="max-w-[250px]" />
+              <h2 className="text-center text-white text-[24px] font-medium">
+                No Local Setup
+              </h2>
+              <p className="text-center text-slate-400 text-[18px] font-normal">
+                Soroban Learn requires no local setup. No need to install any
+                packages or even open your code editor
+              </p>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+            <div className="flex flex-col justify-center items-center text-center gap-5">
+              <Image src={Steps} alt="Steps" className="max-w-[250px]" />
+              <h2 className="text-center text-white text-[24px] font-medium">
+                Step by step instructions
+              </h2>
+              <p className="text-center text-slate-400 text-[18px] font-normal">
+                Soroban Learn requires no local setup. No need to install any
+                packages or even open your code editor
+              </p>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <div className="flex flex-col justify-center items-center text-center gap-5">
+              <Image
+                src={Resources}
+                alt="Resources"
+                className="max-w-[250px]"
+              />
+              <h2 className="text-center text-white text-[24px] font-medium">
+                Post Resources
+              </h2>
+              <p className="text-center text-slate-400 text-[18px] font-normal">
+                Soroban Learn requires no local setup. No need to install any
+                packages or even open your code editor
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
