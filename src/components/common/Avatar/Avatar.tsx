efd0c2fe61ccddfa461c7cc18a93bf6ffeec1e55
Zograb Mkrtchyan <mkrtch.zograb@gmail.com>
2023-07-24 19:10:36 +0400

    ✨ add avatar component

---
+++ src/components/common/Avatar/Avatar.tsx
@@ -0,0 +1,40 @@
+import type { FC } from "react";
+import Image, { StaticImageData, type ImageProps } from "next/image";
+import cx from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+interface AvatarProps extends Omit<ImageProps, 'alt' | 'src'> {
+  src?: StaticImageData;
+}
+
+const Avatar: FC<AvatarProps> = ({ src, className, ...otherProps }) => (
+  <>
+    {src && (
+      <Image
+        src={src}
+        width={40}
+        height={40}
+        alt="Avatar"
+        className={cx(
+          'rounded-full object-cover',
+          className,
+        )}
+        {...otherProps}
+      />
+    )}
+    {!src && (
+      <div
+        className={cx(
+          'w-10 h-10 bg-[#232323] rounded-full flex justify-center items-center',
+          className,
+        )}
+        {...otherProps}
+      >
+        <FontAwesomeIcon icon={faUser} className="text-lg" />
+      </div>
+    )}
+  </>
+);
+
+export default Avatar;
