9da86e3af87c35a56fdf2fbd62d80ef5e5d7c4f1
Zograb Mkrtchyan <mkrtch.zograb@gmail.com>
2023-07-25 23:07:06 +0400

    🔥 remove un-used code

--- src/components/IDE/codeEditor.tsx
+++
@@ -1,7 +0,0 @@
-import useCodeEditor from "./useCodeEditor";
-
-export default function CodeEditor({ value, onChange, extensions }) {
-  const ref = useCodeEditor({ value, onChange, extensions });
-
-  return <div ref={ref} />;
-}
