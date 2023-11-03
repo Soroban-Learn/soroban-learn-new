f50559a0a51ee9e56897c8836a459ed91c0d42db
Jonathon <jonathon@pixel8.io>
2023-11-02 20:00:34 -0400

    ✨ add file explorer component

---
+++ src/components/FileExplorer/FileExplorer.tsx
@@ -0,0 +1,66 @@
+import type { FC } from "react";
+import type { FileStructureNode } from "@/types";
+import Scrollbar from "react-scrollbars-custom";
+import cx from "classnames";
+
+// Components
+import FileExplorerNode from "./FileExplorerNode";
+
+interface FileExplorerProps {
+  nodes: FileStructureNode[];
+  selectedFileId?: string;
+  setSelectedFileId: (fileId: string) => void;
+}
+
+const FileExplorer: FC<FileExplorerProps> = ({
+  nodes,
+  selectedFileId,
+  setSelectedFileId,
+}) => {
+  return (
+    <div className="h-[300px] pb-12 bg-[#232323]">
+      <div className={cx(
+        'flex justify-start items-center h-[50px] px-5',
+        'border-b border-solid border-black font-bold',
+      )}>
+        File Explorer
+      </div>
+      <div className="pt-4 pb-6 pr-3">
+        <div className="h-52">
+          <Scrollbar
+            disableTracksWidthCompensation
+            trackYProps={{
+              renderer: (props) => {
+                const { elementRef, ...restProps } = props;
+                return <span {...restProps} ref={elementRef} className="!bg-black !rounded-md !w-1" />;
+              },
+            }}
+            thumbYProps={{
+              renderer: (props) => {
+                const { elementRef, ...restProps } = props;
+                return <span {...restProps} ref={elementRef} className="!bg-white !w-full !block !rounded-md" />;
+              },
+            }}
+          >
+            <div className="pr-4">
+              {nodes.map((node) => (
+                <FileExplorerNode
+                  key={node.id}
+                  id={node.id}
+                  title={node.title}
+                  type={node.type}
+                  nodes={node.children}
+                  nestedIndex={1}
+                  selectedFileId={selectedFileId}
+                  setSelectedFileId={setSelectedFileId}
+                />
+              ))}
+            </div>
+          </Scrollbar>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FileExplorer;
---
+++ src/components/FileExplorer/FileExplorerNode.tsx
@@ -0,0 +1,107 @@
+import type { FileStructureNode, FileStructureNodeType } from "@/types";
+import { type FC, useState, useCallback } from "react";
+import cx from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { faFile } from "@fortawesome/pro-regular-svg-icons";
+
+export interface FileExplorerFolderProps {
+  id: string;
+  title: string;
+  type: FileStructureNodeType;
+  nodes?: FileStructureNode[];
+  nestedIndex: number;
+  selectedFileId?: string;
+  setSelectedFileId: (fileId: string) => void;
+}
+
+const FileExplorerNode: FC<FileExplorerFolderProps> = ({
+  id,
+  title,
+  type,
+  nodes,
+  nestedIndex,
+  selectedFileId,
+  setSelectedFileId,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = useCallback(() => {
+    if (type === 'folder') {
+      setIsOpen(!isOpen);
+    } else {
+      setSelectedFileId(id);
+    }
+  }, [id, type, isOpen, setSelectedFileId]);
+
+  return (
+    <div className={cx('cursor-pointer', {
+      'mt-2.5': type === 'file',
+      'mt-4': type === 'folder',
+    })}>
+      <div
+        className={cx(
+          'h-[25px] flex justify-start items-center text-xs',
+          'border-l-4 border-transparent border-solid',
+          'transition-all duration-300 ease-linear',
+          'rounded-r-md',
+          {
+            'bg-black border-[#5546FF]': selectedFileId === id,
+          }
+        )}
+        style={{
+          paddingLeft: `${nestedIndex * 1}rem`,
+        }}
+        onClick={handleClick}
+      >
+        {type === "folder" && (
+          <FontAwesomeIcon
+            icon={faCaretDown}
+            className={cx(
+              'transition-all duration-200 ease-in-out',
+              {
+                'rotate-180': !isOpen,
+                'rotate-0': isOpen,
+              }
+            )}
+          />
+        )}
+        {type === "file" && (
+          <FontAwesomeIcon icon={faFile} />
+        )}
+        <div className="ml-3">{title}</div>
+      </div>
+      {!!nodes?.length && (
+        <div
+          className={cx(
+            'grid grid-rows-[0fr] ease-in-out transition-all duration-500',
+            {
+              'grid-rows-[1fr]': isOpen,
+            },
+          )}
+        >
+          <div className="overflow-hidden">
+            {nodes?.map((node) => (
+              <FileExplorerNode
+                key={node.id}
+                id={node.id}
+                title={node.title}
+                type={node.type}
+                nodes={node.children}
+                nestedIndex={nestedIndex + 1}
+                selectedFileId={selectedFileId}
+                setSelectedFileId={setSelectedFileId}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+FileExplorerNode.defaultProps = {
+  nodes: [],
+}
+
+export default FileExplorerNode;
