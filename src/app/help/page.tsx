5708823723d0fc23b1ad63de79694c3befaa7a08
andriAntt <andriy.ant.work@gmail.com>
2023-12-14 15:07:44 +0200

    working on help page

---
+++ src/app/help/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Header from "@/components/Header";
+import { FC } from "react";
+
+const HelpPage: FC = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+    </div>
+  );
+};
+
+export default HelpPage;
---
+++ src/components/HelpPageSidebar/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+import OverviewIcon from "@assets/images/list.svg";
+import OpenRequestsIcon from "@assets/images/message.svg";
+import RequestHistoryIcon from "@assets/images/history.svg";
+import SettingsIcon from "@assets/images/cogs.svg";
+
+export const HelpPageSidebar: FC = () => {
+  return <div>index</div>;
+};
